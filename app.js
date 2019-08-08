ace24b669ddd35dd590d4ab3eb3201c8b4c0dc10
Michael Krog <michaelryankrog@gmail.com>
2019-08-07 21:55:01 -0600

    created put and post requets for notes

--- app.js
+++ app.js
@@ -32,6 +32,51 @@ app.get('/api/v1/notes', (request, response) => {
     });
 });
 
+// Adding Note
+app.post('/api/v1/notes', async (req, res) => {
+  try {
+    const { title, copy, status, order_id } = req.body;
+    if (!title) return res.status(422).json('Please provide a title, copy and status.');
+    const notes = await database('notes').select();
+    const id = await database('notes').insert({ title, copy, status, order_id }, 'id');
+    res.status(201).json({ id });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+// Deleting Note
+app.delete('/api/v1/notes/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const matchingPalettes = await database('notes').where({ id });
+    if (!matchingPalettes.length) return res.sendStatus(404);
+    await database('notes')
+      .where({ id })
+      .del();
+    res.status(204).json('Note Deleted');
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+// Updating Note
+app.put('/api/v1/notes/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, copy, status } = req.body;
+    if (!title) return res.status(422).json('Please provide a title, copy and status.');
+    const matchingNote = await database('notes').where({ id });
+    if (!matchingNote.length) return res.status(404).json('Note not found.');
+    await database('notes')
+      .where({ id })
+      .update({ title, copy, status });
+    res.status(202).json('Title Updated Successfully');
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
 
 
 module.exports = app;
